1142deb07551ceeafa8b84e6d187ee0eaeef4c14
Gabrielle Victoria <gabrielebatista2810@gmail.com>
2024-06-25 11:23:24 -0300

    cadastrar produtos

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from validate_docbr import CPF, CNPJ
 
 app = Flask("Minha App") # nome da minha aplicação/projeto - pode ser qualquer coisa
 cpf = CPF()
 cnpj = CNPJ()
 # página do flask: rota + função
+
+lista_produtos = [
+        {"nome": "Percy Jackson", "descricao":"Mata a sede", "imagem": "https://www.intrinseca.com.br/upload/livros/ladrao-de-raios_243x349.jpg"},
+        {"nome": "O ultimo demonio a morrer", "descricao":"Suja a mão", "imagem": "https://intrinseca.com.br/wp-content/uploads/2024/05/oultimodemonioamorrer-g.jpg"},
+        {"nome": "Os heróris do Olimpo", "descricao":"Bom", "imagem": "https://intrinseca.com.br/wp-content/uploads/2023/05/BoxOsHeroisdoOlimpo-G.jpg"}
+
+    ]
 
 # /home page - página inicial
 @app.route("/")
@@ -19,11 +26,7 @@ def cont():
 # /produtos - página de produtos
 @app.route("/produtos")
 def produtos():
-    lista_produtos = [
-        {"nome": "Percy Jackson", "descricao":"Mata a sede", "imagem": "https://www.intrinseca.com.br/upload/livros/ladrao-de-raios_243x349.jpg"},
-        {"nome": "O ultimo demonio a morrer", "descricao":"Suja a mão", "imagem": "https://intrinseca.com.br/wp-content/uploads/2024/05/oultimodemonioamorrer-g.jpg"},
-        {"nome": "Os heróris do Olimpo", "descricao":"Bom", "imagem": "https://intrinseca.com.br/wp-content/uploads/2023/05/BoxOsHeroisdoOlimpo-G.jpg"}
-    ]
+    
     return render_template("produtos.html", produtos=lista_produtos)
 
 # /produtos - página de produtos
@@ -51,3 +54,20 @@ def plpv():
 @app.route("/utilizacao")
 def utlz():
     return render_template("utilizacao.html")
+
+
+#GET/ produto/cadastro
+
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro_produto.html")
+
+# POST / produstos que vai lidar com os dados
+# acessar request 
+@app.route("/produtos", methods=['POST'])
+def salvar_produto():
+    nome = request.form["nome"]
+    descricao = request.form["descricao"]
+    produto = {"nome": nome, "descricao": descricao, "imagem": ""}
+    lista_produtos.append(produto)
+    return render_template("produtos.html", produtos=lista_produtos)
